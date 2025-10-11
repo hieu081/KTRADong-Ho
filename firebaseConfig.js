5d76a3a76f516063b5ad8f23a15df82d7c750857
hieu081 <hieuminh0945@gmail.com>
2025-10-11 20:40:19 +0700

    HIeu

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+window.firebaseConfig = {
+  apiKey: "AIzaSyDn7GLPt_K-7Os2olo6XgdS4_kTtPKn_ng",
+  authDomain: "ktradong-ho.firebaseapp.com",
+  databaseURL: "https://ktradong-ho-default-rtdb.firebaseio.com",
+  projectId: "ktradong-ho",
+  storageBucket: "ktradong-ho.firebasestorage.app",
+  messagingSenderId: "806158456825",
+  appId: "1:806158456825:web:2a0ee6650e055488cc8f28",
+  measurementId: "G-6NZ03Z84KY",
+};
